c7a696a6d10003f004d64d9cac673b783be9936b
Jeong, Jiung <ethernuiel@sanultari.com>
2012-11-25 01:52:26 +0900

    set timeout for http connection

--- lib/server_status/client.rb
+++ lib/server_status/client.rb
@@ -3,21 +3,28 @@ require 'socket'
 require 'timeout'
 
 class ServerStatusClient
-  def initialize hash
+  def initialize(hash)
     @domain = hash[:domain.to_s]
     @port = hash[:port.to_s]
     @scheme = hash[:ssl.to_s] ? 'https' : 'http'
-    @url = "#{@scheme}://#{@domain}:#{@port}"
+    @url = "#@scheme://#@domain:#@port"
     @default = hash[:apis.to_s][:default.to_s]
   end
 
   def request
-    response = RestClient.get "#{@url}#{@default}"
-    response.code
-  rescue => e
-    e.response.code
+    Timeout::timeout(5) do
+      begin
+        response = RestClient.get "#@url#@default"
+        response.code
+      rescue => e
+        e.response.code
+      end
+    end
+  rescue Timeout::Error
+    return 408
   end
 
+  #noinspection RubyResolve
   def is_port_open?
     Timeout::timeout(1) do
       begin
